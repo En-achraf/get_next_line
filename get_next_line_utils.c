0aba3f3958b63d80429cf86a465dcf1a573280fb
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2025-01-05 18:38:46 +0100

    +++

---
+++ get_next_line_utils.c
@@ -0,0 +1,29 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*str;
+
+	str = malloc(ft_strlen(s1) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
