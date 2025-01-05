1b0e0f7aa8c3f72d0bfe548f3354e88425a364bb
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2025-01-05 18:37:46 +0100

    +++

--- get_next_line.c
+++ get_next_line.c
@@ -2,31 +2,67 @@
 
 char *get_next_line(int fd)
 {
+    static char *saved; 
     char *buffer;
-    char *str;
+    char *line;
+    char *temp;
     int count;
-    
+
     if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
-        
-    buffer = malloc(BUFFER_SIZE + 1);
-    if (!buffer)
-        return (NULL);
-        
-    count = read(fd, buffer, BUFFER_SIZE);
-    if (count <= 0)
+
+    if (!saved)
     {
-        free(buffer);
+        buffer = malloc(BUFFER_SIZE + 1);
+        if (!buffer)
+            return (NULL);
+        count = read(fd, buffer, BUFFER_SIZE);
+        if (count <= 0)
+        {
+            free(buffer);
+            return (NULL);
+        }
+        buffer[count] = '\0';
+        saved = buffer;
+    }
+
+    temp = saved;
+    while (*temp && *temp != '\n')
+        temp++;
+    int sum;
+
+    if(*temp == '\n')
+        sum = 1;
+    else
+        sum = 0;
+
+    int line_len = temp - saved + sum;
+    line = malloc(line_len + 1);
+    if (!line)
         return (NULL);
+
+    int i = 0;
+    while (i < line_len)
+    {
+        line[i] = saved[i];
+        i++;
+    }
+    line[i] = '\0';
+
+    if (*temp == '\n')
+        temp++;
+    
+    buffer = saved;
+    if (*temp)
+    {
+        saved = ft_strdup(temp);
+        free(buffer);
+    }
+    else
+    {
+        free(saved);
+        saved = NULL;
     }
     
-    buffer[count] = '\0';
-    str = malloc(count + 1);
-    if (!str)
-    {
-        free(buffer);
-        return (NULL);
-    }
-    
-    return (str);
+    return (line);
 }
