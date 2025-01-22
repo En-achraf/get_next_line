fc37d76518c3c7595647895d5397800da0dc2985
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2025-01-22 23:50:05 +0100

    Done

--- get_next_line.h
+++ get_next_line.h
@@ -11,18 +11,22 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-# include <fcntl.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 1
+#endif
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
 
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *str);
+int		ft_findchr(char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
