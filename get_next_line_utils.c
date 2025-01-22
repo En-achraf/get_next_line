45be630a794c5af7ec60429c03b584a3f7096b04
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2025-01-22 23:49:36 +0100

    Done

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,6 +12,7 @@
 
 #include "get_next_line.h"
 
+
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
@@ -20,24 +21,6 @@ size_t	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	int		i;
-	char	*str;
-
-	str = malloc(ft_strlen(s1) + 1);
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
 }
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
@@ -58,4 +41,51 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	}
 	dst[i] = '\0';
 	return (len);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*str;
+
+	str = malloc(ft_strlen(s1) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	ft_findchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s && s[i])
+	{
+		if (c == s[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+
+	if (!s1)
+		s1 = ft_strdup("");
+
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);	
+	ft_strlcpy((str + ft_strlen(s1)), (s2), (ft_strlen(s2) + 1));
+	return (str);
 }
