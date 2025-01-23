57f068cf201a630abbde43a07e1309ac66b351f1
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2025-01-23 02:47:16 +0100

    Done

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -9,19 +9,23 @@
 /*   Updated: 2025/01/12 17:02:34 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#define GET_NEXT_LINE_BONUS_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
-# include <fcntl.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 1
+#endif
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
 
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *str);
+int		ft_findchr(char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
