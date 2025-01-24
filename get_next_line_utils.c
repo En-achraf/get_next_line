7dbf477d1f6c89c07467f3fc780ef04c9917f03b
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2025-01-24 10:29:43 +0100

    Done

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -87,5 +87,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	ft_strlcpy(str, s1, ft_strlen(s1) + 1);	
 	ft_strlcpy((str + ft_strlen(s1)), (s2), (ft_strlen(s2) + 1));
+	free(s1);
 	return (str);
 }
